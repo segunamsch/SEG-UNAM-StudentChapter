7cf642be3acf5535b990fbeaa7f7ff6894ef4ffe
segunamsch <segunamsch@gmail.com>
2025-10-08 10:29:26 -0600

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
-<head></head>
+let slideIndex = 1;
+showSlides(slideIndex);
+let autoSlideIndex = 0;
+carouselAuto();
+// Controles para avanzar/retroceder
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Controles para ir a una diapositiva específica (los puntos)
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("carrusel-slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function carouselAuto() {
+  let i;
+  let slides = document.getElementsByClassName("carousel-slide");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  autoSlideIndex++;
+  if (autoSlideIndex > slides.length) {autoSlideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[autoSlideIndex-1].style.display = "block";
+  dots[autoSlideIndex-1].className += " active";
+  setTimeout(carouselAuto, 5000); // Cambia de imagen cada 5 segundos
+}
