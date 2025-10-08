fc760636e84e92fd445d4ffb138b799162a23a70
segunamsch <segunamsch@gmail.com>
2025-10-08 11:17:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let slideIndex = 1;
 showSlides(slideIndex);
-let autoSlideIndex = 0;
-carouselAuto();
+
 // Controles para avanzar/retroceder
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -26,21 +25,4 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-}
-
-function carouselAuto() {
-  let i;
-  let slides = document.getElementsByClassName("carousel-slide");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  autoSlideIndex++;
-  if (autoSlideIndex > slides.length) {autoSlideIndex = 1}
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[autoSlideIndex-1].style.display = "block";
-  dots[autoSlideIndex-1].className += " active";
-  setTimeout(carouselAuto, 5000); // Cambia de imagen cada 5 segundos
 }
